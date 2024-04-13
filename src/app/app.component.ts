d6a3265fc2d20ca6c719031c7de372bd3a746177
kaikeventura <kaike.gnr@hotmail.com>
2024-04-13 16:36:12 -0300

    Dash para links uteis

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatGridList, MatGridTile} from "@angular/material/grid-list";
+import {MatCard, MatCardActions, MatCardHeader, MatCardImage, MatCardTitle} from "@angular/material/card";
+import {MatButtonModule} from "@angular/material/button";
+import {NgForOf, NgOptimizedImage} from "@angular/common";
+import {FlexLayoutModule} from "@angular/flex-layout";
+import {MatToolbar} from "@angular/material/toolbar";
+
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatGridList, MatGridTile, MatCard, MatCardActions, MatButtonModule, NgForOf, MatCardImage, NgOptimizedImage, FlexLayoutModule, MatCardTitle, MatCardHeader, MatToolbar],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'donnie-dash';
+  tiles: Tile[] = [
+    {text: 'Three', cols: 1, rows: 3, color: '#e6e6fa'},
+    {text: 'Three', cols: 1, rows: 3, color: '#e6e6fa'},
+    {text: 'Three', cols: 1, rows: 3, color: '#e6e6fa'},
+    {text: 'Three', cols: 1, rows: 3, color: '#e6e6fa'}
+  ];
 }
